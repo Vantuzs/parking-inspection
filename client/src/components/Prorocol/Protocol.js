09a48ccb619ad21bf82680687016f1e1024c0266
Vantuz <Shcherbina444@gmail.com>
2025-06-21 12:58:09 +0300

    Add slider

--- client/src/components/Prorocol/Protocol.js
+++ client/src/components/Prorocol/Protocol.js
@@ -1,30 +1,50 @@
-import React from 'react';
-import styles from './Protocol.module.scss'
+import React from "react";
+import styles from "./Protocol.module.scss";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const Protocol = ({protocol}) => {
-    return (
-        <article className={styles['card-wrapper']}>
-            <h1>Prtocol №{protocol.id}</h1>
-            
-            <p>Violator: {protocol.violatorFullName}</p>
-            <p>Violator passport: {protocol.violatorPassportNumber}</p>
-            <p>Service notes: {protocol.serviceNote}</p>
-            <h3>Fine amount: {protocol.fineAmount}</h3>
-            <p>Created at: {protocol.createdAt}</p>
-            <p>Updated at: {protocol.updatedAt}</p>
+const Protocol = ({ protocol }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true
+  };
+  return (
+    <article className={styles["card-wrapper"]}>
+      <h1>Prtocol №{protocol.id}</h1>
 
-            <p>Officer: {protocol.ParkOfficer.full_name}</p>
-            <p>Officer badge number: {protocol.ParkOfficer.badge_number}</p>
+      <p>Violator: {protocol.violatorFullName}</p>
+      <p>Violator passport: {protocol.violatorPassportNumber}</p>
+      <p>Service notes: {protocol.serviceNote}</p>
+      <h3>Fine amount: {protocol.fineAmount}</h3>
+      <p>Created at: {protocol.createdAt}</p>
+      <p>Updated at: {protocol.updatedAt}</p>
 
-            {protocol.Images.map(currentImage=> (
-                <img 
-                 key={currentImage.id}
-                 src={`http://localhost:5000/images/${currentImage.path}`}
-                 alt={protocol.id}
-                />
-            ))}
-        </article>
-    );
-}
+      <p>Officer: {protocol.ParkOfficer.full_name}</p>
+      <p>Officer badge number: {protocol.ParkOfficer.badge_number}</p>
+        
+        {protocol.Images.length >1 ?
+        <Slider {...settings} className={styles.slider} >
+      {protocol.Images.map((currentImage) => (
+          <img
+          key={currentImage.id}
+          src={`http://localhost:5000/images/${currentImage.path}`}
+          alt={protocol.id}
+          />
+        ))}
+        </Slider>:protocol.Images.map((currentImage) => (
+          <img
+          key={currentImage.id}
+          src={`http://localhost:5000/images/${currentImage.path}`}
+          alt={protocol.id}
+          />
+        ))}
+    </article>
+  );
+};
 
 export default Protocol;
